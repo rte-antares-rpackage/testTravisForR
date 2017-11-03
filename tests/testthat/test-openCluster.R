0953c6e8d3def88081e21a69ec03fec0e48a4b82
TitouanRobert <titouan.robert.pro@gmail.com>
2017-11-03 12:38:07 +0100

    r

--- tests/testthat/test-openCluster.R
+++ tests/testthat/test-openCluster.R
@@ -2,7 +2,7 @@ context("openCluster")
 library(parallel)
 test_that("openCl",{
   print("Before open")
-  cl <- makeCluster(1)
+  cl <- makeCluster(2)
   print("After open")
   stopCluster(cl)
   print("After cl")

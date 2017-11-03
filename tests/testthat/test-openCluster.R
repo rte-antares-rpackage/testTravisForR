a90fcffe81e05cdc8d298184d59daaee2617c26a
TitouanRobert <titouan.robert.pro@gmail.com>
2017-11-03 11:01:58 +0100

    debug

--- tests/testthat/test-openCluster.R
+++ tests/testthat/test-openCluster.R
@@ -2,7 +2,7 @@ context("openCluster")
 library(parallel)
 test_that("openCl",{
   print("Before open")
-  cl <- parallel::makeCluster(1, type = "MPI")
+  cl <- future::makeClusterPSOCK(1)
   print("After open")
   stopCluster(cl)
   print("After cl")

f4c00beb08e4d57aa1bb1fc8dc2d1fad5277f54f
TitouanRobert <titouan.robert.pro@gmail.com>
2017-11-03 15:01:20 +0100

    makeCluster options

--- tests/testthat/test-openCluster.R
+++ tests/testthat/test-openCluster.R
@@ -2,9 +2,12 @@ context("openCluster")
 library(parallel)
 test_that("openCl",{
   print("Before open")
-  cl <- makePSOCKcluster("localhost", manual=FALSE)
+  cl <- makeCluster(1, revtunnel=TRUE, master="localhost", port=11, homogeneous=FALSE)
+
   print("After open")
   stopCluster(cl)
 
   print("After cl")
 })
+
+
